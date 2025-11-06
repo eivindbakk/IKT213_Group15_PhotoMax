d32b031198fb8aa2cae278884da139e794545d59
Martin Hjulstad Bednar <m.hjulstad@gmail.com>
2025-11-06 16:53:12 +0100

    Save function working

--- MenuHandlers/File.cs
+++ MenuHandlers/File.cs
@@ -1,4 +1,8 @@
+using System;
+using System.IO;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Microsoft.Win32;
 
 namespace PhotoMax
@@ -18,8 +22,64 @@ namespace PhotoMax
                 MessageBox.Show($"TODO: Open image: {dlg.FileName} then SetArtboardSize(imageW, imageH)");
         }
 
-        private void File_Save_Click(object sender, RoutedEventArgs e) => 
-            MessageBox.Show("TODO: Save merged Image + Strokes.");
+        private void File_Save_Click(object sender, RoutedEventArgs e) 
+        {
+            var dlg = new SaveFileDialog
+            {
+                FileName = "Image",
+                DefaultExt = ".png",
+                Filter = "PNG Image (.png)|*.png|JPEG Image (.jpg)|*.jpg;*.jpeg|Bitmap Image (.bmp)|*.bmp|TIFF Image (.tif)|*.tif;*.tiff",
+                Title = "Save Image As"
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                try 
+                {
+                    SaveCanvasToFile(dlg.FileName);
+                    MessageBox.Show("Image saved successfully!", "Save", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show($"Error saving file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void SaveCanvasToFile(string filename)
+        {
+            // Get the size of the canvas
+            double width = Artboard.ActualWidth;
+            double height = Artboard.ActualHeight;
+
+            // Create a render target bitmap to capture the canvas (image + strokes)
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(
+                (int)width, (int)height, 96, 96, PixelFormats.Pbgra32);
+
+            // Render the Artboard (which includes ImageView and PaintCanvas)
+            renderBitmap.Render(Artboard);
+
+            // Choose encoder based on file extension
+            BitmapEncoder encoder;
+            string ext = System.IO.Path.GetExtension(filename).ToLower();
+            
+            encoder = ext switch
+            {
+                ".jpg" or ".jpeg" => new JpegBitmapEncoder { QualityLevel = 95 },
+                ".bmp" => new BmpBitmapEncoder(),
+                ".tif" or ".tiff" => new TiffBitmapEncoder(),
+                _ => new PngBitmapEncoder()
+            };
+
+            encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+
+            // Save to file
+            using (FileStream stream = new FileStream(filename, FileMode.Create))
+            {
+                encoder.Save(stream);
+            }
+        } 
+            
 
         private void File_SaveAs_Click(object sender, RoutedEventArgs e) => 
             MessageBox.Show("TODO: Save As...");
