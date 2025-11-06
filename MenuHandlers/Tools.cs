6042a4d61e09be8bf4e2b8fd736a60be5b12135e
Martin Hjulstad Bednar <m.hjulstad@gmail.com>
2025-11-06 16:29:49 +0100

    fixed error

--- MenuHandlers/Tools.cs
+++ MenuHandlers/Tools.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Ink;
+using System.Windows.Input;
 using System.Windows.Media;
 using WF = System.Windows.Forms;
 
